d9bedcd7655c47941a17e1d6ecd6834437a8dcd2
Harpie94 <Harpie94@outlook.fr>
2022-10-19 18:07:42 +0200

    v2

--- js/index.js
+++ js/index.js
@@ -4,45 +4,50 @@ let categorybtn = document.getElementById('categorySelector')
 let cat1 = document.getElementById('category1')
 let cat2 = document.getElementById('category2')
 let cat3 = document.getElementById('category3')
+let itemList = document.getElementById('Item')
+let bool = false;
 
 submitbtn.addEventListener('click', () =>{
+    let text = txtfield.value;
+    let catValue = categorybtn.value;
+    bool = true;
+//console.log(catValue);
 
-let text = txtfield.value;
-let catValue = categorybtn.value;
-console.log(catValue);
 if(text.length == 0){
-
     console.log("ecrit ton pèwou");
 }
-else if(catValue == "cat1"){
 
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat1.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
-}
-else if(catValue == "cat2"){
-
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat2.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
-}
-else if(catValue == "cat3"){
-
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat3.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
+else
+{
+    createItemList();
 }
 
+function createItemList(){
+    let newItem = document.createElement('li');
+    let delBtn = document.createElement('button');
+    let modBtn = document.createElement('button');
+    let a = 10;
+    newItem.classList.add('Item');
+    delBtn.id = 'delButton';
+    modBtn.id = 'modButton';
+    newItem.innerText = text;
+    eval(catValue).appendChild(newItem);
+    newItem.appendChild(delBtn);
+    newItem.appendChild(modBtn);
+
+    txtfield.value = '';
+    console.log(text);
+
+    return newItem;
+    
+}
 })
+
+// if(bool){
+//     console.log(bool)
+//     .addEventListener('mouseover', hoverFunction)
+// }
+function hoverFunction(){
+    console.log("Hover");
+    this.style.color = 'blue';
+}
