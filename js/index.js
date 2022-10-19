fac2d6b342176222e71ae51338ef3666dc50d800
draknut <victor-elisee.lopez@outlook.fr>
2022-10-19 15:47:07 +0200

    btn

--- js/index.js
+++ js/index.js
@@ -4,24 +4,49 @@ let categorybtn = document.getElementById('categorySelector')
 let cat1 = document.getElementById('category1')
 let cat2 = document.getElementById('category2')
 let cat3 = document.getElementById('category3')
+let itemList = document.getElementById('Item')
+let bool = false;
+
 submitbtn.addEventListener('click', () =>{
     let text = txtfield.value;
     let catValue = categorybtn.value;
+    bool = true;
 //console.log(catValue);
 
-    function createItemList(){
-        let newItem = document.createElement('li');
-        newItem.classList.add('.Item');
-        newItem.innerText = text;
-        console.log(text);
-        eval(catValue).appendChild(newItem);
-        txtfield.value = '';
-    }
-    if(text.length == 0){
-        console.log("ecrit ton pèwou");
-    }
-    else
-    {
-        createItemList();  
-    }
+if(text.length == 0){
+    console.log("ecrit ton pèwou");
+}
+
+else
+{
+    createItemList();
+}
+
+function createItemList(){
+    let newItem = document.createElement('li');
+    let delBtn = document.createElement('button');
+    let modBtn = document.createElement('button');
+    newItem.classList.add('Item');
+    delBtn.id = 'delButton';
+    modBtn.id = 'modButton';
+    newItem.innerText = text;
+    eval(catValue).appendChild(newItem);
+    newItem.appendChild(delBtn);
+    newItem.appendChild(modBtn);
+
+    txtfield.value = '';
+    console.log(text);
+
+    return newItem;
+    
+}
 })
+
+// if(bool){
+//     console.log(bool)
+//     .addEventListener('mouseover', hoverFunction)
+// }
+function hoverFunction(){
+    console.log("Hover");
+    this.style.color = 'blue';
+}
