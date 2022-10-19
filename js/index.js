4fa53e15ddae473f0793464d0fb13b9bc1725208
draknut <victor-elisee.lopez@outlook.fr>
2022-10-19 14:11:52 +0200

    full opti

--- js/index.js
+++ js/index.js
@@ -4,45 +4,24 @@ let categorybtn = document.getElementById('categorySelector')
 let cat1 = document.getElementById('category1')
 let cat2 = document.getElementById('category2')
 let cat3 = document.getElementById('category3')
-
 submitbtn.addEventListener('click', () =>{
+    let text = txtfield.value;
+    let catValue = categorybtn.value;
+//console.log(catValue);
 
-let text = txtfield.value;
-let catValue = categorybtn.value;
-console.log(catValue);
-if(text.length == 0){
-
-    console.log("ecrit ton pèwou");
-}
-else if(catValue == "cat1"){
-
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat1.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
-}
-else if(catValue == "cat2"){
-
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat2.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
-}
-else if(catValue == "cat3"){
-
-    let newItem = document.createElement('li');
-    newItem.classList.add('.Item');
-    newItem.innerText = text;
-    cat3.appendChild(newItem);
-    console.log(text);
-    txtfield.value = '';
-    
-}
-
+    function createItemList(){
+        let newItem = document.createElement('li');
+        newItem.classList.add('.Item');
+        newItem.innerText = text;
+        console.log(text);
+        eval(catValue).appendChild(newItem);
+        txtfield.value = '';
+    }
+    if(text.length == 0){
+        console.log("ecrit ton pèwou");
+    }
+    else
+    {
+        createItemList();  
+    }
 })
